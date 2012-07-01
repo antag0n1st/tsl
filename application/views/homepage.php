4ae0100f9e16edaba9a1ae4ef8bd451d3fd2275e
Antagonist <Antagonist@Antagonist-PC>
2012-07-01 21:14:23 +0200

    head , menu

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,8 +1,25 @@
 <div class="container">
-    The Content of the page
-    <p>this is not a conflict , it should be only a MERGE</p>
-    <h1>Title Le Change</h1>
-    <div>
-        CHANGES MADE BY VLADO 2
+    <div class="flexslider">
+        <ul class="slides">
+            <li>
+                <img src="<?php echo base_url() . 'images/slider/experta.jpg'; ?>" />
+                <p class="flex-caption">Captions and cupcakes. Winning combination.</p>
+            </li>
+            <li>
+                <a href="#"><img src="<?php echo base_url() . 'images/slider/inacup_pumpkin.jpg'; ?>" /></a>
+                <p class="flex-caption">This image is wrapped in a link!</p>
+            </li>
+            <li>
+                <img src="<?php echo base_url() . 'images/slider/inacup_donut.jpg'; ?>" />
+            </li>
+            <li>
+                <img src="<?php echo base_url() . 'images/slider/inacup_vanilla.jpg'; ?>" />
+            </li>
+        </ul>
     </div>
 </div>
+<script type="text/javascript">
+    $(window).load(function() {
+        $('.flexslider').flexslider();
+    });
+</script>
