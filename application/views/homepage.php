7202c05264f12a22679d27ef0a1ece2ab30ca443
Antagonist <Antagonist@Antagonist-PC>
2012-07-01 18:03:42 +0200

    initial

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,5 +1,6 @@
 <div class="container">
     The Content of the page
+    <p>this is not a conflict , it should be only a MERGE</p>
     <h1>Title Le Change</h1>
     <div>
         CHANGES MADE BY IKO
