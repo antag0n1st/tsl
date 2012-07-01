78eae4c2c552f9276cce0a37310da5bf0cbcd5a4
Antagonist <Antagonist@Antagonist-PC>
2012-07-01 21:45:50 +0200

    latest news

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,27 +1,43 @@
-<div class="container">
-    <div class="flexslider">
-        <ul class="slides">
-            <li>
-                <img src="<?php echo base_url() . 'images/slider/experta.jpg'; ?>" />
-                <p class="flex-caption">Captions and cupcakes. Winning combination.</p>
-            </li>
-            <li>
-                <a href="#"><img src="<?php echo base_url() . 'images/slider/inacup_pumpkin.jpg'; ?>" /></a>
-                <p class="flex-caption">This image is wrapped in a link!</p>
-            </li>
-            <li>
-                <img src="<?php echo base_url() . 'images/slider/inacup_donut.jpg'; ?>" />
-            </li>
-            <li>
-                <img src="<?php echo base_url() . 'images/slider/inacup_vanilla.jpg'; ?>" />
-            </li>
-        </ul>
-    </div>
-</div>
+<?php $this->load->view('elements/slidebar'); ?>
 <div class="container o" style="margin-top: 30px;">
 
     <div class="left">
-        The content of the page
+     
+            <h3>Најнови вести</h3>
+            <div class="latest-news">
+                <div class="thumb">
+                    <a href="http://elegantthemes.com/preview/Evolution/2008/09/05/ut-sagittis-ultrices-urna-eget-erat-non-purus-varius/">
+                        <img src="http://elegantthemes.com/preview/Evolution/wp-content/uploads/et_temp/b-7-131549_60x60.jpg" class="r-post-image" alt="Ut sagitti’s ultrices urna eget erat non" width="60px" height="60px">										<span class="overlay"></span>
+                    </a>
+                </div> 	<!-- end .thumb -->
+
+                <p class="date">September 5, 2012 | </p>
+                <h4 class="title"><a href="http://elegantthemes.com/preview/Evolution/2008/09/05/ut-sagittis-ultrices-urna-eget-erat-non-purus-varius/">Ut sagitti’s ultrices urna eget erat non</a></h4>
+                <p>Sed tempor lectus in risus. Nullam vestibulum, odio ac pSed tempor lectus in risus. Nullam vestibulum, odio ac pulvinarulvinar condimentum, urna Sed tempor lectus in risus. Nullam vestibulum, odio ac pulvinarneque aliquet ipsum, in blandit...</p>
+            </div> <!-- end .r-post -->
+            <div class="latest-news">
+                <div class="thumb">
+                    <a href="http://elegantthemes.com/preview/Evolution/2008/09/04/hello-world-2/">
+                        <img src="http://elegantthemes.com/preview/Evolution/wp-content/uploads/et_temp/b-8-150556_60x60.jpg" class="r-post-image" alt="Neque porro quisquam est qui" width="60px" height="60px">										<span class="overlay"></span>
+                    </a>
+                </div> 	<!-- end .thumb -->
+
+                <p class="date">September 4, 2012 | </p>
+                <h4 class="title"><a href="http://elegantthemes.com/preview/Evolution/2008/09/04/hello-world-2/">Neque porro quisquam est qui</a></h4>
+                <p>Sed ut perspiciatis unde omnis iste nSed tempor lectus in risus. Nullam vestibulum, odio ac pulvinaratus error sit voluSed tempor lectus in risus. Nullam vestibulum, odio ac pulvinarptatem accusantium doloremque laudantium, totam rem...</p>
+            </div> <!-- end .r-post -->
+            <div class="latest-news">
+                <div class="thumb">
+                    <a href="http://elegantthemes.com/preview/Evolution/2008/08/05/nam-libero-tempore-cum-soluta-nobis-est-2/">
+                        <img src="http://elegantthemes.com/preview/Evolution/wp-content/uploads/et_temp/b-9-108029_60x60.jpg" class="r-post-image" alt="Nam libero tempore, cum soluta nobis" width="60px" height="60px">										<span class="overlay"></span>
+                    </a>
+                </div> 	<!-- end .thumb -->
+
+                <p class="date">August 5, 2012 | </p>
+                <h4 class="title"><a href="http://elegantthemes.com/preview/Evolution/2008/08/05/nam-libero-tempore-cum-soluta-nobis-est-2/">Nam libero tempore, cum soluta nobis</a></h4>
+                <p>Nam libero tempore, cum soluta nobis estSed tempor lectus in risus. Nullam vestibulum, odio ac pulvinar eligendi Sed tempor lectus in risus. Nullam vestibulum, odio ac pulvinaroptio cumque nihil impedit quo minus id quod maxime placeat...</p>
+            </div> <!-- end .r-post -->
+
     </div>
 
     <div class="right">
@@ -30,8 +46,3 @@
 
 
 </div>
-<script type="text/javascript">
-    $(window).load(function() {
-        $('.flexslider').flexslider();
-    });
-</script>
