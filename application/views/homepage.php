8637d03e199619982cf522e054a96692aa3f4476
Antagonist <Antagonist@Antagonist-PC>
2012-07-02 23:57:38 +0200

    background texture

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -38,6 +38,18 @@
                 <h4 class="title"><a href="#">Nam libero tempore, cum soluta nobis</a></h4>
                 <p>Nam libero tempore, cum soluta nobis estSed tempor lectus in risus. Nullam vestibulum, odio ac pulvinar eligendi Sed tempor lectus in risus. Nullam vestibulum, odio ac pulvinaroptio cumque nihil impedit quo minus id quod maxime placeat...</p>
             </div> <!-- end .r-post -->
+            
+            <div class="latest-news">
+                <div class="thumb">
+                    <a href="#">
+                        <img src="<?php echo base_url().'images/uploads/b-8-150556_60x60.jpg'; ?>" alt="" width="60px" height="60px">										<span class="overlay"></span>
+                    </a>
+                </div> 
+
+                <p class="date">September 4, 2012 | </p>
+                <h4 class="title"><a href="#">Neque porro quisquam est qui</a></h4>
+                <p>Sed ut perspiciatis unde omnis iste nSed tempor lectus in risus. Nullam vestibulum, odio ac pulvinaratus error sit voluSed tempor lectus in risus. Nullam vestibulum, odio ac pulvinarptatem accusantium doloremque laudantium, totam rem...</p>
+            </div> 
 
     </div>
 
