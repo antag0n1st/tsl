1a5a86d1bfb545db88641bc643a04f1b86f32bee
Antagonist <Antagonist@Antagonist-PC>
2012-07-07 14:12:07 +0200

    menu

--- application/views/admin/articles/articles.php
+++ application/views/admin/articles/articles.php
@@ -1,27 +1,30 @@
 <div class="container o">
     <h3>Сите статии</h3>
 
-    <div style="margin:20px 0px 0px 0px">    
+    <div class="article-grid-holder">    
 <?php foreach($articles as $article): ?>
-<div style="width:950px">
-    <div style="float:left;width:240px;height:100px;overflow:hidden;">
+<div class="article-grid-row">
+    <div class="article-grid-thumbnail">
         <a href="<?php echo base_url();?>admin/articles/edit_article/<?php echo $article->id; ?>">
             <img src="<?php echo base_url(); ?>public/uploaded/featured/thumbnails/<?php echo $article->featured_image; ?>" alt="" width="240" />
         </a>
     </div>
-    <div style="padding:0 0 0 10px;float:left;width:350px;height:70px">
+    <div class="article-grid-title">
         <a href="<?php echo base_url();?>admin/articles/edit_article/<?php echo $article->id; ?>">
             <?php echo $article->title; ?>
         </a>
     </div>
-    <div style="float:left;width:120px;height:70px">
+    <div class="article-grid-date">
         <?php echo date("d.m.Y H:i", strtotime($article->date_published) ); ?>
     </div>
-    <div style="float:left;width:80px;height:70px">
+    <div class="article-grid-status">
         <?php FieldHelper::status_field($article->status); ?>
     </div>
-    <div style="float:left;width:125px">
-        <a href="<?php echo base_url();?>admin/articles/edit_article/<?php echo $article->id; ?>">Измени</a> | <a href="#">Избриши</a>
+    <div class="article-grid-edit-links">
+        <a href="<?php echo base_url();?>admin/articles/edit_article/<?php echo $article->id; ?>" title="Измени">
+        <img src="<?php echo base_url()?>public/images/edit_pencil.png" alt="" style="width:24px" />
+        </a> | 
+        <a href="#" class="delete-link" title="Избриши" rel="<?php echo $article->id; ?>"><img src="<?php echo base_url()?>public/images/delete_red.png" alt="" style="width:24px"  /></a>
     </div>
     <?php echo $article->id; ?>
     <div class="clear"></div>
@@ -33,3 +36,9 @@
     </div>
 <?php echo $this->pagination->create_links(); ?>
 </div>
+
+<script type="text/javascript">
+    $(".delete-link").click(function(){
+        alert('TODO: Delete this article');
+    });
+</script>
