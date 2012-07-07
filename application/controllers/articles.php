fcffec81328d8b535f6705e65bfed6c0be9318e1
Antagonist <Antagonist@Antagonist-PC>
2012-07-07 20:05:16 +0200

    article view

--- application/controllers/articles.php
+++ application/controllers/articles.php
@@ -4,8 +4,22 @@ class Articles extends MY_Controller {
 
 	public function index($article_id = 0)
 	{
-            $data['main_content'] = 'article';
-            $this->load->view('layout/layout',$data);
+            
+                $this->load->model('articles_model');
+                $events = $this->articles_model->get_events();                
+                $event_categories = $this->articles_model->get_event_categories();
+                
+                $article = $this->articles_model->get_articles(array('id' => $article_id));
+                if(isset($article[0])){
+                    $article = $article[0];
+                }
+                
+                $data['article'] = $article;
+                $data['events'] = $events;
+                $data['event_categories'] = $event_categories;                 
+                $data['main_content'] = 'article';
+                
+                $this->load->view('layout/layout',$data);
         }
         
         
--- application/views/article.php
+++ application/views/article.php
@@ -1,1 +1,20 @@
-<h2>This page with display article details</h2>
+<div class="container o" style="margin-top: 10px;">
+
+    <div class="left">
+        <div class="img-container">
+            <img alt="" src="<?php echo base_url().'public/uploaded/featured/'.$article->featured_image; ?>" />
+        </div>
+        <br />
+            <h3><?php echo $article->title; ?></h3>
+            <div class="content">
+                <?php echo $article->content; ?>
+            </div>
+            
+    </div>
+
+    <div class="right">
+        <?php $this->load->view('elements/sidebar'); ?>
+    </div>
+
+
+</div>
