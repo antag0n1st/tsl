b5428b9bd3266a90ca161e784b44d268f53aa710
Vladimir <Vladimir@Vladimir-PC>
2012-07-15 15:22:50 +0200

    done feedback changes on the homepage

--- application/views/elements/header.php
+++ application/views/elements/header.php
@@ -1,11 +1,19 @@
 <div class="container" style="padding: 10px 5px 0px 5px;">
-    <div class="search-box">
-        <input type="text" value="пребарувај..." />
-        <span class="search-button"></span>
-        <h3>Delivering success…</h3>
+    <div class="logo-holder float-left">
+        <a href="<?php echo base_url(); ?>" style="text-decoration: none;">
+            <img style="border: none;" alt="tsl logo" src="<?php echo base_url() . 'images/logo.png'; ?>" />
+        </a>
     </div>
-    <a href="<?php echo base_url(); ?>" style="text-decoration: none;">
-        <img style="border: none;" alt="tsl logo" src="<?php echo base_url() . 'images/logo.png'; ?>" />
-    </a>
-    <br />
+    <div class="header-panel-holder float-right">
+        <div class="float-right">
+            <h3>Delivering success…</h3>
+        </div>
+        <div class="clear"></div>
+        <div class="search-box">
+            <input type="text" value="пребарувај..." />
+            <span class="search-button"></span>
+        </div>
+        <div class="clear"></div>
+    </div>
+    <div class="clear"></div>
 </div>
