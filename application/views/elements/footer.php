7d5e1092727b7d1c4e5b8942aba92792388b3995
Vladimir <Vladimir@Vladimir-PC>
2012-07-21 14:18:42 +0200

    added footer control to the users

--- application/views/elements/footer.php
+++ application/views/elements/footer.php
@@ -6,47 +6,6 @@
 
     <div class="separator" style="border-color: #747474;"></div>
     <?php echo $footer->content; ?>
-    
-    <!--<div class="left" style="background-image: none;">
-        <h3 style="color: white;">Контакт</h3>
-        <p>
-            Доколку не можете да го пронајдете тоа што Ве интересира или имате дополнително прашање, контактирајте нè на:
-
-        </p>
-        <br />
-        <div style="float: left; width: 50%;margin: 0px;">
-            <p>
-            <b>Triple S Group doo</b>
-            <br />
-            Ул. Орце Николов бр. 190-3/5
-            <br />
-            1000 Скопје, 
-            <br />
-            Р.Македонија
-            </p>
-        </div>
-        <div style="float: left; width: 50%;margin: 0px;">
-            <p>
-            <b>тел.:</b> 02/ 3 112 048
-            <br />
-            <b>факс:</b> 02/ 3 112 395
-            <br />
-            <b>жиро сметка:</b> 530010100496686
-            <br />
-            <b>депонент:</b> Охридска Банка, АД Охрид
-            <br />
-            <b>даночен број:</b> МК 4030005533741
-            </p>
-        </div>
-
-    </div>
-    <div class="right" style="background-image: none;border-color: #747474;">
-        <a href="https://maps.google.com/maps?q=42.006054,21.407708&hl=en&num=1&t=h&z=18" target="_blank" rel="nofollow">
-            <img alt="" src="http://maps.googleapis.com/maps/api/staticmap?center=42.00605,21.407711&zoom=15&size=300x206&sensor=false&maptype=hybrid&markers=color:red%7Ccolor:red%7C42.00605,21.407711" />
-        </a>
-        
-    </div>-->
-
 </div>
 <div class="container" >
     <div class="separator" style="border-color: #747474;"></div>
