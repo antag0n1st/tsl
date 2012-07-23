a2bec516f6982ffd9921dad3d58cd2f8bfd189d5
Vladimir <Vladimir@Vladimir-PC>
2012-07-23 22:07:43 +0200

    -added social buttons in color

--- application/views/pages_of_success.php
+++ application/views/pages_of_success.php
@@ -3,7 +3,7 @@
 
     <div class="left">
      
-            <h3>Pages of Success</h3>
+            <h3>Страници на успех</h3>
             <?php if($finished_newsletters): ?>
             <table class="table">
                 <thead>
