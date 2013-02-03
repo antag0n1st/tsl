7eaf353fc1d240b7425480732dea3b80f5d35762
Vladimir <Vladimir@Vladimir-PC>
2013-02-03 11:54:47 +0100

    adding a download link for gallery images

--- application/views/gallery_browse.php
+++ application/views/gallery_browse.php
@@ -11,7 +11,7 @@
             <div class="gallery">
                 <?php foreach($photos as $photo): ?>
                 <div class="photo">
-                    <a href="<?php echo base_url().'public/uploaded/gallery/'.$photo->image; ?>" rel="lightbox[group]" title="<?php echo $photo->description; echo '&nbsp;&lt;a href=&quot;' . base_url().'public/uploaded/gallery/'.$photo->image . '&quot; target=&quot;_blank&quot;   &gt;' . 'Линк за до сликата&lt;/a&gt;'; ?>  "><img style="width: 132px;" alt="" src="<?php echo base_url().'public/uploaded/gallery/thumbnails/'. $photo->image; ?>" /></a>
+                    <a href="<?php echo base_url().'public/uploaded/gallery/'.$photo->image; ?>" rel="lightbox[group]" title="<?php echo $photo->description; echo '&nbsp;&lt;a href=&quot;' . base_url().'public/uploaded/gallery/'.$photo->image . '&quot; target=&quot;_blank&quot;   &gt;' . 'Линк до сликата&lt;/a&gt;'; ?>  "><img style="width: 132px;" alt="" src="<?php echo base_url().'public/uploaded/gallery/thumbnails/'. $photo->image; ?>" /></a>
                    
                 </div>
                 <?php endforeach; ?>                
